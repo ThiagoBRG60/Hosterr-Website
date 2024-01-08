9311566039149278ac2203d98bb90fe3e009f87c
Thiago Correa <thiagocorrea200375@gmail.com>
2024-01-08 03:17:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("hamburguer-menu");
+const menuList = document.getElementById("hamburguer-list");
+
+function hamburguer() {
+  menuList.classList.toggle("selected");
+}
